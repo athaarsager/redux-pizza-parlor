24a779cab45435121768d31faf3a56b92c0c6b69
Andrew Haarsager <athaarsager@gmail.com>
2024-02-01 09:47:30 -0600

    Created pizzaReducer and cartReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,26 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
 // Be sure to replace this reducer! 🙂
-const someReducer = (state = [], action) => {
+const pizzaReducer = (state = [], action) => {
+  if (action.type === "GET_PIZZAS") {
+    return [...state, action.payload];
+  } 
+  return state;
+}
+
+const cartReducer = (state = [], action) => {
+  if (action.type === "ADD_TO_CART") {
+    return [...state, action.payload];
+  } else if (action.type === "REMOVE_FROM_CART") {
+    return state.filter(pizza => pizza.id !== action.payload);
+  }
   return state;
 }
 
 const store = createStore(
   combineReducers({
-    someReducer, // 👈 Be sure to replace this, too!
+    pizzas: pizzaReducer, // 👈 Be sure to replace this, too!
+    cart: cartReducer
   }),
   applyMiddleware(logger),
 );

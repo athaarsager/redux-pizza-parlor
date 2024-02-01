6684dccbdca5b394dd5a5583cc6c094e1cdd8020
Andrew Haarsager <athaarsager@gmail.com>
2024-02-01 10:01:59 -0600

    Created orderReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 
-// Be sure to replace this reducer! 🙂
+// for displaying all the pizzas
 const pizzaReducer = (state = [], action) => {
   if (action.type === "GET_PIZZAS") {
     return [...state, action.payload];
@@ -9,6 +9,7 @@ const pizzaReducer = (state = [], action) => {
   return state;
 }
 
+// cart-related activities
 const cartReducer = (state = [], action) => {
   if (action.type === "ADD_TO_CART") {
     return [...state, action.payload];
@@ -18,10 +19,19 @@ const cartReducer = (state = [], action) => {
   return state;
 }
 
+// this fires when someone hits "checkout." Adds order to admin page
+const orderReducer = (state = [], action) => {
+  if (action.type === "ADD_TO_ORDERS") {
+    return [...state, action.payload];
+  }
+  return state;
+}
+
 const store = createStore(
   combineReducers({
     pizzas: pizzaReducer, // 👈 Be sure to replace this, too!
-    cart: cartReducer
+    cart: cartReducer,
+    orders: orderReducer
   }),
   applyMiddleware(logger),
 );

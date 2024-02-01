8de2c995f36294bad99d67b35ac7b5eb7455460e
Andrew Haarsager <athaarsager@gmail.com>
2024-02-01 10:12:42 -0600

    Added Checkout component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import Checkout from '../Checkout/Checkout';
 
 function App() {
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
+        <h1 className='App-title'>Live, Leavitt, Pizza</h1>
       </header>
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      <Checkout />
     </div>
   );
 }

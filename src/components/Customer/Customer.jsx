0849811e00a78c8696eb2070b87f2467fae0e44f
Seid Redi <seidredi7@gmail.com>
2024-02-01 13:56:22 -0600

    added form for customer

---
+++ src/components/Customer/Customer.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useState } from "react"
+import { useDispatch } from "react-redux";
+export default function Order(){
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [zip, setZip] = useState('');
+    const [deliveryType,setDeliveryType] = useState('pickup');
+    const dispatch = useDispatch();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        dispatch({
+            type:'ADD_CURRENT_USER',
+            payload: {
+                customer_name: name,
+                street_address: address,
+                city: city,
+                zip: zip,
+                type: deliveryType,
+            }
+        })
+    }
+
+    function handleDeliveryType(event) {
+        if (event.target.value === 'pickup') {
+            setDeliveryType('pickup');
+            
+        }
+        else{
+            setDeliveryType('delivery');
+        }
+        
+    }
+    return <>
+        <h1>Step 2: Customer Information</h1>
+        <form onSubmit={handleSubmit}>
+            <input value={name} onChange={e=>{setName(e.target.value)}} placeholder="Name" type="text" />
+            <input value={address} onChange={e=>{setAddress(e.target.value)}} placeholder="Street Address" type="text" />
+            <input value={city} onChange={e=>{setCity(e.target.value)}} placeholder="City" type="text" />
+            <input value={zip} onChange={e=>setZip(e.target.value)} placeholder="Zip" type="text" />
+
+
+            <div>
+                <input value="pickup" checked={deliveryType === 'pickup'} type="radio" name="pickup" id="pickup" onChange={handleDeliveryType} />
+                <label htmlFor="pickup">Pickup</label>
+            </div>
+            <div>
+                <input type="radio" name="Delivery" id="Delivery" checked={deliveryType === 'delivery'} onChange={handleDeliveryType}/>
+                <label htmlFor="delivery">Delivery</label>
+            </div>
+
+            <button>Submit</button>
+        </form>
+        
+    </>
+}

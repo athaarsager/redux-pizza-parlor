bf52385a0ce7b6e6f014dba97cfa4cfb4cbfc21e
Dylan Brown <dylanbrown588@gmail.com>
2024-02-01 13:26:54 -0600

    Make the remove conditions and make sure it works

--- src/components/PizzaListItem/PizzaListItem.jsx
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -1,15 +1,31 @@
 import { useDispatch } from "react-redux"
+import { useState } from "react";
 
 function PizzaListItem({pizza}) {
     const dispatch = useDispatch();
+    const [isInCart, setIsInCart] = useState(false);
+
+    const priceFormatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    });
 
     const addToCart = () => {
         console.log(pizza);
 
-        dispatch({
-            type: "ADD_TO_CART",
-            payload: pizza
-        })
+        if(!isInCart) {
+            dispatch({
+                type: "ADD_TO_CART",
+                payload: pizza
+            })
+        setIsInCart(true);
+        } else {
+            dispatch({
+                type: "REMOVE_FROM_CART",
+                payload: pizza.id
+            })
+        setIsInCart(false)
+        }
     }
 
     return(
@@ -17,8 +33,10 @@ function PizzaListItem({pizza}) {
             <img src={pizza.image_path} alt={pizza.description}/>
             <h2>{pizza.name}</h2>
             <p>{pizza.description}</p>
-            <h5>{pizza.price}</h5>
-            <button onClick={addToCart}>ADD</button>
+            <h5>{priceFormatter.format(pizza.price)}</h5>
+            <button onClick={addToCart}>
+            {isInCart ? "REMOVE" : "ADD"}
+            </button>
         </div>
     )
 }

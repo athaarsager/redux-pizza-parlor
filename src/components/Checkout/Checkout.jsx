9e35db541f1ea12f967e013a4e0a95e72945dd3b
Andrew Haarsager <athaarsager@gmail.com>
2024-02-01 10:53:26 -0600

    Imported currentUser store

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,4 +1,5 @@
 import Header from "../Header/Header";
+import { useSelector } from "react-redux";
 function Checkout() {
     // NEED: DISPLAY CART ON PAGE IN TABLE FORMAT. JUST NAME AND COST OF EACH PIZZA
     // NEED: DISPLAY USER INFO AS PER README
@@ -7,9 +8,14 @@ function Checkout() {
             // SHOW CONFIRMATION DIALOG -- SWEETALERT TIME!
             // NAVIGATE USER BACK TO SELECT PIZZA PAGE
             // CLLEAR REDUCERS AS APPROPRIATE
+    const currentUser = useSelector(store => store.currentUser);
     return (
         <>
         <Header />
+        <h2>Step 3: Checkout</h2>
+        <p>{currentUser.customer_name}</p>
+        <p>{currentUser.street_address}</p>
+        <p>{currentUser.city}</p>
         </>
     )
 }

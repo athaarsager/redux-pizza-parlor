de4138a9b1c8d59c78ad59b1b644f64d1f6930a2
Andrew Haarsager <athaarsager@gmail.com>
2024-02-02 13:14:13 -0600

    Installed moment, Adjusted date display on AdminRow.jsx

--- src/components/Admin/AdminRow.jsx
+++ src/components/Admin/AdminRow.jsx
@@ -1,8 +1,9 @@
+import moment from "moment";
 function AdminRow({ order }) {
     return (
         <tr>
             <td>{order.customer_name}</td>
-            <td>{order.time}</td>
+            <td>{moment(order.time).format("MMMM Do YYYY, h:mm A")}</td>
             <td>{order.type}</td>
             <td>{order.total}</td>
         </tr>

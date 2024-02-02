68d805cde57786c33b62313861e5733b5c376159
Seid Redi <seidredi7@gmail.com>
2024-02-02 13:20:35 -0600

    - updated list item to check if an item is in cart or not. - customer can go back to the home screen from customer page - selected pizzas are reserved and shown on screen of the home paage when the back button is clicked

--- src/components/PizzaListItem/PizzaListItem.jsx
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -1,34 +1,45 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useState } from "react";
 
 function PizzaListItem({pizza}) {
     const dispatch = useDispatch();
-    const [isInCart, setIsInCart] = useState(false);
+    const cart = useSelector(s=>s.cart);
+    // const [isInCart, setIsInCart] = useState(false);
 
     const priceFormatter = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD"
     });
 
+    function isInCart(pizza) {
+        // console.log("cart pizza", cart);
+        let pizzaFound = cart.filter((pizzaInCart) => pizzaInCart.id === pizza.id);
+        console.log("found pizza", pizzaFound);
+        return pizzaFound.length != 0? true : false;
+    }
+
     const addToCart = () => {
-        console.log(pizza);
-        if(!isInCart) {
-            dispatch({
-                type: "ADD_TO_CART",
-                payload: pizza
-            })
-            
-        setIsInCart(true);
-        } else {
+        // console.log(pizza);
+        if(isInCart(pizza)) {
             dispatch({
                 type: "REMOVE_FROM_CART",
                 payload: pizza.id
             })
+            
+        // setIsInCart(true);
+        } else {
+            dispatch({
+                type: "ADD_TO_CART",
+                payload: pizza
+            })
            
-        setIsInCart(false)
+        // setIsInCart(false)
         }
     }
 
+    
+    
+    console.log(isInCart(pizza));
     return(
         <div key={pizza.id}>
             <img src={pizza.image_path} alt={pizza.description}/>
@@ -36,7 +47,7 @@ function PizzaListItem({pizza}) {
             <p>{pizza.description}</p>
             <h5>{priceFormatter.format(pizza.price)}</h5>
             <button onClick={addToCart}>
-            {isInCart ? "REMOVE" : "ADD"}
+            {isInCart(pizza)? "REMOVE" : "ADD"}
             </button>
         </div>
     )
